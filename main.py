d4903851db4d8f03fdf4e04240d2604a9af811dc
MaikeDsc <maquiavelcampos@gmail.com>
2026-02-02 20:55:43 -0400

    adicionado interações com o usuário

--- main.py
+++ main.py
@@ -9,7 +9,19 @@ def lembrete():
         timeout = 10, 
     )
 
+print("/" * 60)
+print("-- bem vindo ao Water reminder! --".center(60, '/'))
+print("/" * 60)
+
+print("\nQual o intevalo de tempo pata receber as notificações? \n")
+
+# converte os minutos em segundos para o time.sleep
+minutos = int (input("Digite os minutos desejados de intevalo aqui : "))
+tempo  = minutos * 60
+
+
 #eviará a notificação a cada 45 minutos
+print(f"\nNotificações configuradas para cada {minutos} minutos. \n-- Water reminder ativo ! -- ")
 while True:
     lembrete()
-    time.sleep(45*60)
+    time.sleep(tempo)
